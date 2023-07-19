360bd4128e300cd3298318e2eea0c55618635963
Samriddhi Singh <108535666+Samriddhii-Siingh@users.noreply.github.com>
2023-07-20 01:01:28 +0530

    Added Navbar

---
+++ frontend/src/Components/Basics/Contact.js
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './contact.css';
+
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    source: 'friends', 
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Send the form data to the backend API
+      const response = await axios.post('http://localhost:5000/api/contact', formData);
+      console.log(response.data);
+      // Reset the form after successful submission
+      setFormData({
+        name: '',
+        email: '',
+        source: 'friends',
+        message: '',
+      });
+      alert('Form submitted successfully!');
+    } catch (error) {
+      console.log(error);
+      alert('Form submission failed. Please try again later.');
+    }
+  };
+
+  return (
+    <>
+      <div className="container-fluid  contact22">
+        <h1 className="contact">CONTACT US</h1>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <div className="row g">
+          <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 px-5   divi">
+            <div className="backimg22"></div>
+          </div>
+          <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 px-5">
+            <form onSubmit={handleSubmit}>
+              <label>Name</label>
+              <br></br>
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="enter your name"
+                className="form-control"
+              ></input>
+              <br></br>
+              <br></br>
+              <label>Email</label>
+              <br></br>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="enter your email"
+                className="form-control"
+              ></input>
+              <br></br>
+              <br></br>
+              <label>How did you find us</label>
+              <br></br>
+              <select
+                name="source"
+                value={formData.source}
+                onChange={handleChange}
+                className="form-control"
+              >
+                <option value="friends">friends</option>
+                <option value="search">search</option>
+                <option value="advertisement">advertisement</option>
+                <option value="other">other</option>
+              </select>
+              <br></br>
+              <br></br>
+              <label>Drop us a line</label>
+              <br></br>
+              <input
+                type="textarea"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                className="form-control  feedback"
+                placeholder="your Message"
+              ></input>
+              <br></br>
+              <br></br>
+              <div className="text-center">
+                <button type="submit" className="btn btn-primary btn-lg book">
+                  Send
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
---
+++ frontend/src/Components/Basics/Features.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import './features.css'
+
+ 
+const Features = () => {
+  return (
+
+    <>
+    <h1 className="Restaurents"> Awsome features</h1>
+
+    <div className="container-fluid" >
+    
+    
+    
+    
+      <div className="row ">
+    
+    <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 px-5" > 
+
+
+    <div className="card text-center">
+     
+  <div className="card-body">
+    <h4 className="card-title">365 DAYS/YEAR</h4>
+    <p className="card-text">Never cook again! We really mean that. Our subscription plans include up to 365 days/year of coverage and you can also choose to order more flexibly if that is your style.</p>
+    
+  </div>
+   
+</div>
+</div>
+
+<div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 px-5" > 
+
+
+<div className="card text-center">
+ 
+<div className="card-body">
+<h4 className="card-title">365 DAYS/YEAR</h4>
+<p className="card-text">Never cook again! We really mean that. Our subscription plans include up to 365 days/year of coverage and you can also choose to order more flexibly if that is your style.</p>
+
+</div>
+
+</div>
+</div>
+
+
+<div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 px-5" > 
+
+
+<div className="card text-center">
+ 
+<div className="card-body">
+<h4 className="card-title">365 DAYS/YEAR</h4>
+<p className="card-text">Never cook again! We really mean that. Our subscription plans include up to 365 days/year of coverage and you can also choose to order more flexibly if that is your style.</p>
+
+</div>
+
+</div>
+</div>
+
+
+
+
+ 
+
+
+
+ 
+
+
+
+    
+    </div>
+    </div>
+
+    </>
+  )
+}
+
+export default Features
